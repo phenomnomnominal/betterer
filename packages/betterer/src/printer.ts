2ee5a8425eae66ed0a47fafbf9962a384bec74bc
Justin Bennett <zephraph@gmail.com>
2019-10-16 20:08:34 -0400

    (feat :sparkles:) - show error messages in snapshot

--- packages/betterer/src/printer.ts
+++ packages/betterer/src/printer.ts
@@ -6,26 +6,35 @@ export function print(results: BettererResults): string {
   const printed = Object.keys(results).map(resultName => {
     let { timestamp, value } = results[resultName];
     return `\nexports[\`${resultName}\`] = { timestamp: ${timestamp}, value: \`${formatJSON(
-      value
+      value,
+      true
     )}\` };`;
   });
   return [RESULTS_HEADER, ...printed].join('');
 }
 
-function formatJSON(json: string): string {
+function formatJSON(json: string, recurse = false): string {
   let parsed: string | object = json;
   try {
     parsed = JSON.parse(json);
     if (typeof parsed !== 'object') return parsed;
     if (Array.isArray(parsed)) {
-      parsed = `[\n` + parsed.map(v => JSON.stringify(v)).join(',\n') + `\n]`;
+      parsed = parsed
+        .map(v => (recurse ? formatJSON(JSON.stringify(v)) : JSON.stringify(v)))
+        .join(',\n');
+      parsed = `[\n${parsed}\n]`;
     } else {
-      parsed =
-        '{\n' +
-        Object.entries(parsed)
-          .map(([key, value]) => `"${key}":${JSON.stringify(value)}`)
-          .join(',\n') +
-        '\n}';
+      parsed = Object.entries(parsed)
+        .map(
+          ([key, value]) =>
+            `"${key}":${
+              recurse
+                ? formatJSON(JSON.stringify(value))
+                : JSON.stringify(value)
+            }`
+        )
+        .join(',\n');
+      parsed = `{\n${parsed}\n}`;
     }
   } catch {
   } finally {
