a2608870cb58597eb9aa15037c919daedae21e46
Craig Spence <craigspence0@gmail.com>
2019-10-28 16:00:20 +0100

    (docs 📚) - fix scroll handler

--- docs/corner/corner.js
+++ docs/corner/corner.js
@@ -1,6 +1,6 @@
 async function main() {
   const response = await fetch('/betterer/corner/corner.html');
   const text = await response.text();
-  document.body.innerHTML += text;
+  document.body.appendChild(text);
 }
 main();
