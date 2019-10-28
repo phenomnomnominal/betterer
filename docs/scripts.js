84d144214dcbc19d39bc7b2c897339265b613472
Craig Spence <craigspence0@gmail.com>
2019-10-28 15:56:32 +0100

    Fix scroll handler?

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,3 +1,8 @@
-const learnMore = document.getElementById('learn-more');
-const what = document.getElementById('what');
-learnMore.addEventListener('click', () => window.scroll(0, what.offsetTop));
+document.addEventListener('DOMContentLoaded', () => { 
+  const learnMore = document.getElementById('learn-more');
+  const what = document.getElementById('what');
+  learnMore.addEventListener('click', () => {
+    window.scroll(0, what.offsetTop);
+  });
+});
+
