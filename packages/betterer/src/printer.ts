0965e8b129d31c2f00812fb91463f4cbc12f1d79
Justin Bennett <zephraph@gmail.com>
2019-11-21 04:31:43 -0500

    Ensure serialized ts errors can be read back in (#26)
    * Ensure serialised ts errors can be read back in
    
    * Move unescaped chars to const, update snapshots

--- packages/betterer/src/printer.ts
+++ packages/betterer/src/printer.ts
@@ -1,10 +1,14 @@
 import { BettererResults } from './types';
+import { escape } from 'safe-string-literal';
 
 const RESULTS_HEADER = `// BETTERER RESULTS V1.`;
 
 type Printable = {
   print: () => string;
 };
+
+/** Characters that we avoid escaping to make snapshots easier to visually diff */
+const UNESCAPED = '"\n';
 
 export function print(results: BettererResults): string {
   const printed = Object.keys(results).map(resultName => {
@@ -16,8 +20,10 @@ export function print(results: BettererResults): string {
           ? (value as Printable).print()
           : JSON.stringify(value);
     }
-    printedValue = (printedValue as string).replace(/`/g, '\\`');
-    return `\nexports[\`${resultName}\`] = {\n  timestamp: ${timestamp},\n  value: \`${printedValue}\`\n};`;
+    return `\nexports[\`${resultName}\`] = {\n  timestamp: ${timestamp},\n  value: \`${escape(
+      printedValue as string,
+      UNESCAPED
+    )}\`\n};`;
   });
 
   return [RESULTS_HEADER, ...printed].join('');
