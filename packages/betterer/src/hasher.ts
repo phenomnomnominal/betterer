171430fd99aa5ee39620fccadffd532713bf9594
Craig Spence <craigspence0@gmail.com>
2019-12-19 15:03:28 +0100

    maybe fix up hashing?

--- packages/betterer/src/hasher.ts
+++ packages/betterer/src/hasher.ts
@@ -1,7 +1,10 @@
 import { createHash } from 'crypto';
+import { EOL } from 'os';
+
+const DEFAULT_NEW_LINE = '\n';
 
 export function hash(value: unknown): string {
   const hasher = createHash('sha256');
-  hasher.update(JSON.stringify(value), 'utf8');
+  hasher.update(JSON.stringify(value).replace(EOL, DEFAULT_NEW_LINE), 'utf8');
   return hasher.digest('hex');
 }
