23f7a7681bf5147088b84dbdaa04b58a5c79c4cb
Craig Spence <craigspence0@gmail.com>
2019-12-19 18:25:06 +0100

    check hash input length

--- packages/betterer/src/hasher.ts
+++ packages/betterer/src/hasher.ts
@@ -1,5 +1,6 @@
 import * as djb2a from 'djb2a';
 
 export function hash(value: unknown): string {
+  console.log(`${JSON.stringify(value).length}`);
   return djb2a(JSON.stringify(value)).toString();
 }
