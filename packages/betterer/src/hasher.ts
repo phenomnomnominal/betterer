450ad485f27ae3bc17a59f3c2d8eafc605cb3ee5
Craig Spence <craigspence0@gmail.com>
2019-12-19 19:11:59 +0100

    adding some shit

--- packages/betterer/src/hasher.ts
+++ packages/betterer/src/hasher.ts
@@ -1,6 +1,7 @@
 import * as djb2a from 'djb2a';
 
 export function hash(value: unknown): string {
+  console.log(JSON.stringify(value).length);
   console.log(new Set(JSON.stringify(value)));
   return djb2a(JSON.stringify(value)).toString();
 }
