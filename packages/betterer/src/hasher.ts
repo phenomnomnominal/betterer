68c02b1734b5eeb7a90d2f71ea24c9d282b0bb92
Craig Spence <craigspence0@gmail.com>
2019-12-19 19:33:05 +0100

    new line fix v2

--- packages/betterer/src/hasher.ts
+++ packages/betterer/src/hasher.ts
@@ -1,9 +1,12 @@
 import * as djb2a from 'djb2a';
 
+const NEW_LINES = /\r\n|\r|\n/g;
+
 export function hash(value: unknown): string {
   console.log(JSON.stringify(value).length);
   console.log(
     JSON.stringify(value)
+      .replace(NEW_LINES, '\n')
       .split('')
       .map(a => a.charCodeAt(0))
   );
