e6161e2d66930cedb0409bc55d70b0ad60c3c14c
Craig Spence <craigspence0@gmail.com>
2019-12-19 19:23:28 +0100

    ...

--- packages/betterer/src/hasher.ts
+++ packages/betterer/src/hasher.ts
@@ -3,7 +3,9 @@ import * as djb2a from 'djb2a';
 export function hash(value: unknown): string {
   console.log(JSON.stringify(value).length);
   console.log(
-    Array.from(new Set(JSON.stringify(value))).map(a => a.charCodeAt(0))
+    JSON.stringify(value)
+      .split('')
+      .map(a => a.charCodeAt(0))
   );
   return djb2a(JSON.stringify(value)).toString();
 }
