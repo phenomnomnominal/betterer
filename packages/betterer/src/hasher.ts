c49cb6a45c408034c62f75cc8959110619cde266
Craig Spence <craigspence0@gmail.com>
2019-12-19 15:07:17 +0100

    Revert "maybe fix up hashing?"
    This reverts commit 171430fd99aa5ee39620fccadffd532713bf9594.

--- packages/betterer/src/hasher.ts
+++ packages/betterer/src/hasher.ts
@@ -1,10 +1,7 @@
 import { createHash } from 'crypto';
-import { EOL } from 'os';
-
-const DEFAULT_NEW_LINE = '\n';
 
 export function hash(value: unknown): string {
   const hasher = createHash('sha256');
-  hasher.update(JSON.stringify(value).replace(EOL, DEFAULT_NEW_LINE), 'utf8');
+  hasher.update(JSON.stringify(value), 'utf8');
   return hasher.digest('hex');
 }
