8b50e029bc564fd540fbcd35e9dcbbe4335b0d5d
Craig Spence <craigspence0@gmail.com>
2019-12-19 19:40:38 +0100

    zxcvzxcv

--- packages/betterer/src/hasher.ts
+++ packages/betterer/src/hasher.ts
@@ -3,12 +3,8 @@ import * as djb2a from 'djb2a';
 const NEW_LINES = /\r\n|\r|\n/g;
 
 export function hash(value: unknown): string {
-  console.log(JSON.stringify(value).length);
-  console.log(
-    JSON.stringify(value)
-      .replace(NEW_LINES, '\n')
-      .split('')
-      .map(a => a.charCodeAt(0))
-  );
-  return djb2a(JSON.stringify(value)).toString();
+  const result = JSON.stringify(value);
+  console.log(result.length);
+  console.log(result.replace(NEW_LINES, '\n').length);
+  return djb2a(result.replace(NEW_LINES, '\n')).toString();
 }
