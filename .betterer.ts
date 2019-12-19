1d94b3ebff8577eb5ce676f87a6ead284d1fd9ab
Craig Spence <craigspence0@gmail.com>
2019-12-19 19:53:41 +0100

    fixing stuff

--- .betterer.ts
+++ .betterer.ts
@@ -1,13 +1,13 @@
 import { regexpBetterer } from '@betterer/regexp';
-// import { tsqueryBetterer } from '@betterer/tsquery';
+import { tsqueryBetterer } from '@betterer/tsquery';
 
 export default {
   'no hack comments': regexpBetterer(
     './packages/**/src/**/*.ts',
     /(\/\/\s*HACK)/i
+  ),
+  'no raw console.log': tsqueryBetterer(
+    './tsconfig.json',
+    'CallExpression > PropertyAccessExpression[expression.name="console"][name.name="log"]'
   )
-  // 'no raw console.log': tsqueryBetterer(
-  //   './tsconfig.json',
-  //   'CallExpression > PropertyAccessExpression[expression.name="console"][name.name="log"]'
-  // )
 };
