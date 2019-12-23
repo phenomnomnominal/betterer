f152a13705dde3afdd8bc6f770c87d4cbc5607aa
Craig Spence <craigspence0@gmail.com>
2019-12-23 02:11:00 +0100

    random start on watch mode

---
+++ packages/betterer/src/watcher/ignores.ts
@@ -0,0 +1,12 @@
+export const WATCH_IGNORES = [
+  '.log',
+  '.sql',
+  '.sqlite',
+  '.DS_Store',
+  '.Spotlight-V100',
+  '.Trashes',
+  'ehthumbs.db',
+  'Thumbs.db',
+  '.gitignore',
+  'package-lock.json'
+];
