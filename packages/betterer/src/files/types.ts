7d0029c65a25a02790ef652ee92cfa5c286fe04b
Craig Spence <craigspence0@gmail.com>
2020-05-07 03:37:51 +1200

    (feat :sparkles:) - include issue text hash in snapshot

--- packages/betterer/src/files/types.ts
+++ packages/betterer/src/files/types.ts
@@ -2,7 +2,7 @@ import { BettererLoggerCodeInfo } from '@betterer/logger';
 
 export type BettererFileInfo = BettererLoggerCodeInfo;
 export type BettererFileInfoMap = Record<string, Array<BettererFileInfo>>;
-export type BettererFileMark = [number, number, number, string];
+export type BettererFileMark = [number, number, number, string, string];
 export type BettererFileMarks = Array<BettererFileMark>;
 export type BettererFileMarksMap = Record<string, BettererFileMarks>;
 export type BettererFileHashMap = Record<string, string>;
--- packages/betterer/src/hasher.ts
+++ packages/betterer/src/hasher.ts
@@ -1,8 +1,8 @@
 import * as djb2a from 'djb2a';
 
+const NEW_LINE = '\n';
 const NEW_LINES = /\r\n|\r|\n/g;
-const NORMALISED_NEW_LINE = '\n';
 
-export function hash(value: string): string {
-  return djb2a(value.replace(NEW_LINES, NORMALISED_NEW_LINE)).toString();
+export function createHash(value: string): string {
+  return djb2a(value.replace(NEW_LINES, NEW_LINE)).toString();
 }
