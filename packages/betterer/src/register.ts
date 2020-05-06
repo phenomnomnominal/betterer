bda639cf66be196c281ae1f171befe7ca07e136d
Craig Spence <craigspence0@gmail.com>
2020-05-07 03:23:45 +1200

    (fix :bug:) - fix for handling TS files with ES import syntax (#73)
    Should fix #65
    
    I can see this biting me in the face later 😅

--- packages/betterer/src/register.ts
+++ packages/betterer/src/register.ts
@@ -1,13 +1,19 @@
 import { register } from 'ts-node';
 
-import { RESULTS_EXTENTION, JS_EXTENSION } from './constants';
+export const JS_EXTENSION = '.js';
+export const RESULTS_EXTENTION = '.results';
 
 export function registerExtensions(): void {
   // Get the original JS module require:
   const JS = require.extensions[JS_EXTENSION];
 
   // Use TS-Node register to allow `.betterer.ts` config files:
-  register();
+  register({
+    transpileOnly: true,
+    compilerOptions: {
+      module: 'commonjs'
+    }
+  });
 
   // Force `.betterer.results` files to be loaded as JS:
   require.extensions[RESULTS_EXTENTION] = (m: NodeModule, filePath: string): void => {
