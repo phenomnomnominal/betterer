fda655ab9cb401f1abc78f5e1cca194dd21c631f
Craig Spence <craigspence0@gmail.com>
2020-05-16 11:51:55 +1200

    (fix :bug:) - past-Craig did bad copy pasting (#89)

--- packages/typescript/src/errors.ts
+++ packages/typescript/src/errors.ts
@@ -2,7 +2,7 @@ import { registerError } from '@betterer/errors';
 
 export const CONFIG_PATH_REQUIRED = registerError(
   () =>
-    "For `@betterer/tsquery` to work, you need to provide the path to a tsconfig.json file, e.g. `'./tsconfig.json'`. ❌"
+    "For `@betterer/typescript` to work, you need to provide the path to a tsconfig.json file, e.g. `'./tsconfig.json'`. ❌"
 );
 export const COMPILER_OPTIONS_REQUIRED = registerError(
   () => 'For `@betterer/typescript` to work, you need to provide compiler options, e.g. `{ strict: true }`. ❌'
