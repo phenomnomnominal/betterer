b6b3e7dd2a51939ebe82bb2ec8f68897061fe519
Craig Spence <craigspence0@gmail.com>
2020-05-17 22:57:19 +1200

    (tests :rotating_light:) - add test for when config file changes and makes the result change

--- test/betterer-better.spec.ts
+++ test/betterer-better.spec.ts
@@ -43,4 +43,64 @@ module.exports = {
 
     await cleanup();
   });
+
+  it('should work when a test changes and makes the results better', async () => {
+    const { logs, paths, readFile, cleanup, resolve } = await createFixture('test-betterer-better-change-test', {
+      '.betterer.ts': `
+import { tsqueryBetterer } from '@betterer/tsquery';
+
+export default {
+  'no raw console calls': tsqueryBetterer(
+    './tsconfig.json',
+    'CallExpression > PropertyAccessExpression[expression.name="console"]'
+  )
+};  
+      `,
+      '.betterer.changed.ts': `
+import { tsqueryBetterer } from '@betterer/tsquery';
+
+export default {
+  'no raw console calls': tsqueryBetterer(
+    './tsconfig.json',
+    'CallExpression > PropertyAccessExpression[expression.name="console"][name.name="log"]'
+  )
+};
+              `,
+      'tsconfig.json': `
+{
+  "compilerOptions": {
+    "noEmit": true,
+    "lib": ["esnext"],
+    "moduleResolution": "node",
+    "target": "ES5",
+    "typeRoots": ["../../node_modules/@types/"],
+    "resolveJsonModule": true
+  },
+  "include": ["./src/**/*", ".betterer.ts"]
+}      
+      `,
+      'src/index.ts': `
+console.log('foo');
+console.info('foo');
+console.log('foo');
+      `
+    });
+    const resultsPath = paths.results;
+
+    const firstRun = await betterer({ configPaths: [resolve('.betterer.ts')], resultsPath });
+
+    expect(firstRun.new).toEqual(['no raw console calls']);
+
+    const secondRun = await betterer({ configPaths: [resolve('.betterer.changed.ts')], resultsPath });
+
+    expect(secondRun.better).toEqual(['no raw console calls']);
+
+    expect(logs).toMatchSnapshot();
+
+    const result = await readFile(resultsPath);
+
+    expect(result).toMatchSnapshot();
+
+    await cleanup();
+  });
 });
