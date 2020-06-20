04dd60152dffea66f1e9633c3b4958d2d725b703
Craig Spence <craigspence0@gmail.com>
2020-06-20 22:03:09 +1200

    Sort keys before printing file result (#109)

--- packages/betterer/src/test/file-test/printer.ts
+++ packages/betterer/src/test/file-test/printer.ts
@@ -3,20 +3,22 @@ import { BettererFileIssuesMapSerialised } from './types';
 
 export function printer(_: BettererRun, serialised: BettererFileIssuesMapSerialised): string {
   let printed = '{\n';
-  Object.keys(serialised).forEach((filePath, index) => {
-    if (index !== 0) {
-      printed += ',\n';
-    }
-    printed += `    "${filePath}": [\n`;
-    serialised[filePath].forEach((mark, index) => {
-      if (index !== 0) {
-        printed += ',\n';
-      }
-      const [line, column, length, message, hash] = mark;
-      printed += `      [${line}, ${column}, ${length}, ${JSON.stringify(message)}, ${JSON.stringify(hash)}]`;
+  Object.keys(serialised)
+    .sort()
+    .forEach((filePath, index) => {
+      const file = `    "${filePath}": [\n`;
+      printed += prependNewline(index, file);
+      serialised[filePath].forEach((mark, index) => {
+        const [line, column, length, message, hash] = mark;
+        const issue = `      [${line}, ${column}, ${length}, ${JSON.stringify(message)}, ${JSON.stringify(hash)}]`;
+        printed += prependNewline(index, issue);
+      });
+      printed += `\n    ]`;
     });
-    printed += `\n    ]`;
-  });
   printed += '\n  }';
   return printed;
+}
+
+function prependNewline(index: number, str: string): string {
+  return `${index === 0 ? '' : ',\n'}${str}`;
 }
