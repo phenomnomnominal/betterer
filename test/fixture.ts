961cfb8e68a260998af8e78c7651d0cbca553df3
Craig Spence <craigspence0@gmail.com>
2020-09-01 10:56:51 +1200

    WIP

--- test/fixture.ts
+++ test/fixture.ts
@@ -1,6 +1,9 @@
-import { createFixtureDirectoryΔ, FixtureFactory } from '@betterer/fixture';
+import { createFixtureDirectoryΔ, FixtureFileSystemFiles, Fixture } from '@betterer/fixture';
 import * as path from 'path';
 
-export const createFixture = async (): Promise<FixtureFactory> => {
-  return await createFixtureDirectoryΔ(path.resolve(__dirname, '../fixtures'));
-};
+const fixtureFactory = createFixtureDirectoryΔ(path.resolve(__dirname, '../fixtures'));
+
+export async function createFixture(fixtureName: string, files: FixtureFileSystemFiles): Promise<Fixture> {
+  const factory = await fixtureFactory;
+  return factory(fixtureName, files);
+}
