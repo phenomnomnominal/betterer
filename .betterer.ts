2229b937b83a2a0d1a416a0a61aa42460665f3b5
Jonas <jonas.sprenger@invision.de>
2020-10-04 05:26:13 +0200

    fix(eslint 🐛): fix restrict-template-expressions (#115) (#306)
    Co-authored-by: Jonas <sprenger@bryter.io>

--- .betterer.ts
+++ .betterer.ts
@@ -5,7 +5,6 @@ export default {
   'no hack comments': regexp(/(\/\/\s*HACK)/i).include('./packages/**/src/**/*.ts'),
 
   'new eslint rules': eslint({
-    '@typescript-eslint/restrict-template-expressions': 2,
     '@typescript-eslint/no-floating-promises': 2
   }).include('./packages/**/src/**/*.{js,ts}', './test/**/*.{js,ts}', './*.{js,ts}')
 };
--- packages/cli/src/errors.ts
+++ packages/cli/src/errors.ts
@@ -1,6 +1,6 @@
 import { registerError } from '@betterer/errors';
 
-export const COULDNT_WRITE_CONFIG_FILE = registerError((configPath) => `could not read "${configPath}". 😔`);
+export const COULDNT_WRITE_CONFIG_FILE = registerError((configPath) => `could not read "${configPath as string}". 😔`);
 export const COULDNT_FIND_PACKAGE_JSON = registerError(() => `could not find "package.json". 😔`);
 export const COULDNT_READ_PACKAGE_JSON = registerError(() => `could not read "package.json". 😔`);
 export const COULDNT_WRITE_PACKAGE_JSON = registerError(() => `could not write "package.json". 😔`);
