620550cbe121913ec73e55f4c610868c5c4b0de1
Craig Spence <craigspence0@gmail.com>
2020-10-08 16:25:52 +1300

    chore(betterer 🕸): remove check for completed eslint rules

--- .betterer.ts
+++ .betterer.ts
@@ -2,9 +2,5 @@ import { eslint } from '@betterer/eslint';
 import { regexp } from '@betterer/regexp';
 
 export default {
-  'no hack comments': regexp(/(\/\/\s*HACK)/i).include('./packages/**/src/**/*.ts'),
-
-  'new eslint rules': eslint({
-    '@typescript-eslint/no-floating-promises': 2
-  }).include('./packages/**/src/**/*.{js,ts}', './test/**/*.{js,ts}', './*.{js,ts}')
+  'no hack comments': regexp(/(\/\/\s*HACK)/i).include('./packages/**/src/**/*.ts')
 };
