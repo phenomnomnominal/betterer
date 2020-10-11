a370db65ad82466e70028cf8e8a656a4c39b60d5
Craig Spence <craigspence0@gmail.com>
2020-10-11 21:19:09 +1300

    Merge branch 'v4' into improve-logging

--- packages/cli/src/init.tsx
+++ packages/cli/src/init.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import commander from 'commander';
 import { render } from 'ink';
 
 import { Init } from './init/components';
@@ -7,7 +6,7 @@ import { initOptions } from './options';
 import { BettererCLIArguments } from './types';
 
 export async function initΔ(cwd: string, argv: BettererCLIArguments): Promise<void> {
-  const { config } = initOptions(commander, argv);
+  const { config } = initOptions(argv);
 
   render(<Init config={config} cwd={cwd} />);
 }
