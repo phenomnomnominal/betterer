8d96d03eeb1ebd0cb27d897ab4d599c976ccb599
Craig Spence <craigspence0@gmail.com>
2020-11-22 10:08:49 +1100

    build(betterer 🔨): include .tsx files (#410)

--- .betterer.ts
+++ .betterer.ts
@@ -2,5 +2,7 @@ import { eslint } from '@betterer/eslint';
 import { regexp } from '@betterer/regexp';
 
 export default {
-  'no hack comments': regexp(/(\/\/\s*HACK)/i).include('./packages/**/src/**/*.ts')
+  'no hack comments': regexp(/(\/\/\s*HACK)/i)
+    .include('./packages/**/src/**/*.ts')
+    .include('./packages/**/src/**/*.tsx')
 };
