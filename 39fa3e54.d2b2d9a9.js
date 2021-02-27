a69fadc978179ca941d496e3a0fbbc5e0ba90dd5
Craig Spence <craigspence0@gmail.com>
2021-02-27 17:43:50 +0100

    Deploy website - based on f32ee3a5206938927eba0c5ceb5b607aad24f3e5

--- 39fa3e54.d2b2d9a9.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{82:function(e){e.exports=JSON.parse('{"permalink":"/betterer/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
