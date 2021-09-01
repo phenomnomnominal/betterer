b2dc8770ad4e20689a888624157c4a68be4c765e
Craig Spence <craigspence0@gmail.com>
2021-09-01 10:02:41 +0200

    docs(results 📚): adding docs to results package (#847)

--- packages/results/src/index.ts
+++ packages/results/src/index.ts
@@ -1,5 +1,15 @@
-export { write } from './fs';
-export { merge } from './merge';
-export { parse } from './parse';
-export { print } from './print';
+/**
+ * {@link https://www.npmjs.com/package/@betterer/results | `@betterer/results` }
+ *
+ * Results file helpers used within {@link https://github.com/phenomnomnominal/betterer | **Betterer**}.
+ *
+ * 🚨 THIS PACKAGE SHOULD ONLY BE USED WITHIN THE BETTERER MONOREPO 🚨
+ *
+ * @packageDocumentation
+ */
+
+export { writeResults__ } from './fs';
+export { mergeResults__ } from './merge';
+export { parseResults__ } from './parse';
+export { printResults__ } from './print';
 export { BettererResult, BettererResults } from './types';
--- packages/results/src/merge.ts
+++ packages/results/src/merge.ts
@@ -1,6 +1,12 @@
 import { requireText } from './require';
 import { BettererResults } from './types';
 
-export function merge(ours: string, theirs: string): BettererResults {
+/**
+ * Takes two strings of JavaScript, evaluates their contents, and merges the results,
+ * with the latter taking precedence.
+ *
+ * @internal This could change at any point! Please don't use!
+ */
+export function mergeResults__(ours: string, theirs: string): BettererResults {
   return { ...requireText(ours), ...requireText(theirs) };
 }
