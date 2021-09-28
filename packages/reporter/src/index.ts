446043367f4654e8281436883278db4947fd4859
Craig Spence <craigspence0@gmail.com>
2021-09-28 23:57:42 +0200

    docs(reporter 📚): adding docs to reporter package (#863)

--- packages/reporter/src/index.ts
+++ packages/reporter/src/index.ts
@@ -1,1 +1,9 @@
+/**
+ * {@link https://www.npmjs.com/package/@betterer/reporter | `@betterer/reporter` }
+ *
+ * The default reporter for {@link https://github.com/phenomnomnominal/betterer | **Betterer**}.
+ *
+ * @packageDocumentation
+ */
+
 export { reporter } from './reporter';
