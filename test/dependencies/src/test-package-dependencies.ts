2f85267132c4cfa2024fc4c89a235f681999de35
Craig Spence <craigspence0@gmail.com>
2021-12-06 00:21:39 +0100

    build(betterer 🔨): add dependency check test

---
+++ test/dependencies/src/test-package-dependencies.ts
@@ -0,0 +1,41 @@
+import { BettererError } from '@betterer/errors';
+import { BettererLogger } from '@betterer/logger';
+import dependencyCheck from 'dependency-check';
+import { promises as fs } from 'fs';
+import * as path from 'path';
+
+const EXCLUDED_PACKAGES = ['docgen', 'extension', 'fixture'];
+const PACKAGES_DIR = path.resolve(__dirname, '../../../packages');
+
+export async function getPackages(): Promise<Array<string>> {
+  const items = await fs.readdir(PACKAGES_DIR);
+
+  const testDirectory = await Promise.all(
+    items.map(async (item) => {
+      const stat = await fs.lstat(path.join(PACKAGES_DIR, item));
+      return stat.isDirectory();
+    })
+  );
+
+  const packages = items.filter((_, index) => testDirectory[index]);
+  return packages.filter((packageName) => {
+    return packageName && !EXCLUDED_PACKAGES.includes(packageName);
+  });
+}
+
+export async function run(logger: BettererLogger, packageName: string): Promise<string> {
+  const packageNameFull = `@betterer/${packageName}`;
+  await logger.progress(`Validating dependencies for "${packageNameFull}" ...`);
+
+  const parsed = await dependencyCheck({
+    path: require.resolve(packageNameFull),
+    entries: ['package.json']
+  });
+  const missing = await dependencyCheck.missing(parsed.package, parsed.used);
+
+  if (!missing.length) {
+    return `No missing dependencies found in "${packageNameFull}".`;
+  }
+
+  throw new BettererError(`Missing dependencies found in "${packageNameFull}": ${missing.join(', ')}`);
+}
