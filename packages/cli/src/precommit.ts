c993667b788e2328c3a1742870b343934dfffa70
Craig Spence <craigspence0@gmail.com>
2022-01-30 18:10:11 +0100

    fix(betterer 🐛): precommit should allow tests to get better (#954)

--- packages/cli/src/precommit.ts
+++ packages/cli/src/precommit.ts
@@ -34,7 +34,7 @@ export function precommit(cwd: string): Command {
       // And then cast to BettererOptionsStart. This is possibly invalid,
       // but it's nicer to do the options validation in @betterer/betterer
       const suiteSummary = await betterer(options as BettererOptionsStart);
-      if (suiteSummary.changed.length > 0 || suiteSummary.failed.length > 0) {
+      if (suiteSummary.worse.length > 0 || suiteSummary.failed.length > 0) {
         process.exitCode = 1;
       }
     } catch {
