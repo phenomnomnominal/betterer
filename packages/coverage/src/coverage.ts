65c802802d31d70e1f27ccd95ee1cd9fcbfcf169
Lucas Treffenstädt <lucas.treffenstaedt@check24.de>
2022-02-24 09:25:43 +0100

    chore: move helpers to main file

--- packages/coverage/src/coverage.ts
+++ packages/coverage/src/coverage.ts
@@ -22,8 +22,22 @@ import {
   IstanbulFileCoverage,
   Maybe
 } from './types';
-import { reduceJoinRecords, uncoveredFile } from './helpers';
 import { BettererError } from '@betterer/errors';
+
+export const uncoveredFile: BettererCoverageIssue = {
+  lines: Number.MAX_SAFE_INTEGER,
+  statements: Number.MAX_SAFE_INTEGER,
+  functions: Number.MAX_SAFE_INTEGER,
+  branches: Number.MAX_SAFE_INTEGER
+};
+
+export function reduceJoinRecords<
+  T,
+  K1 extends number | string | symbol = string,
+  K2 extends number | string | symbol = string
+>(union: Record<K1, T>, nextRecord: Record<K2, T>): Record<K1 | K2, T> {
+  return { ...union, ...nextRecord };
+}
 
 class BettererCoverageTestΩ
   implements BettererTestOptionsComplex<BettererCoverageIssues, BettererCoverageIssues, BettererCoverageDiff>
