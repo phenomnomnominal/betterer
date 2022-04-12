00bf17d361f291564e0ca179f89c94603d264fc1
Craig Spence <craigspence0@gmail.com>
2022-04-12 17:00:25 +0200

    bug(betterer 🐛): fix global includes again

--- test/include-global.spec.ts
+++ test/include-global.spec.ts
@@ -9,39 +9,50 @@ describe('betterer', () => {
 import { regexp } from '@betterer/regexp';
 
 export default {
-  test: () => regexp(/(\\/\\/\\s*HACK)/i).include('./src/index.ts')
-};      
+  test: () => regexp(/(\\/\\/\\s*HACK)/i).include('src/*.ts')
+};
       `
     });
 
     const configPaths = [paths.config];
     const resultsPath = paths.results;
     const indexPath = resolve('./src/index.ts');
-    const globalPath = resolve('./src/global.ts');
+    const globalValidPath = resolve('./src/global.ts');
+    const globalInvalidPath = resolve('./global.ts');
 
     await writeFile(indexPath, '// Hack');
-    await writeFile(globalPath, '// Hack');
 
-    await betterer({ configPaths, cwd: paths.cwd, resultsPath, workers: false });
+    const localIncludeSummary = await betterer({ configPaths, cwd: paths.cwd, resultsPath, workers: false });
+
+    const [localIncludeRun] = localIncludeSummary.runs;
+
+    expect(localIncludeRun.filePaths).toEqual([indexPath]);
 
     const result = await readFile(resultsPath);
 
     expect(result).toMatchSnapshot();
 
+    await writeFile(globalValidPath, '// Hack');
+    await writeFile(globalInvalidPath, '// Hack');
+
     const globalIncludeSummary = await betterer({
       configPaths,
       cwd: paths.cwd,
-      includes: ['./src/global.ts'],
+      includes: [globalValidPath, globalInvalidPath],
       resultsPath,
       workers: false,
       update: true
     });
 
-    expect(globalIncludeSummary.filePaths).toEqual([globalPath]);
+    expect(globalIncludeSummary.filePaths).toEqual([globalInvalidPath, globalValidPath]);
 
-    const globalExcludeResult = await readFile(resultsPath);
+    const [globalIncludeRun] = globalIncludeSummary.runs;
 
-    expect(globalExcludeResult).toMatchSnapshot();
+    expect(globalIncludeRun.filePaths).toEqual([globalValidPath]);
+
+    const globalIncludeResult = await readFile(resultsPath);
+
+    expect(globalIncludeResult).toMatchSnapshot();
 
     expect(logs).toMatchSnapshot();
 
