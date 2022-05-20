70c215899112712a5e6a2a66c12d041728f98254
Craig Spence <cspence@spotify.com>
2022-05-20 14:46:23 +0200

    bug(betterer 🐛): timer should not render in CI and precommit

--- packages/reporter/src/components/suite/Suite.tsx
+++ packages/reporter/src/components/suite/Suite.tsx
@@ -13,10 +13,11 @@ export interface SuiteProps {
 }
 
 export const Suite: FC<SuiteProps> = memo(function Runs({ context, suite, suiteSummary, done }) {
+  const { ci, precommit } = context.config;
   return (
     <>
       <Box flexDirection="column" paddingBottom={1}>
-        <BettererTasksLogger name="Betterer" update={update} exit={false} done={done}>
+        <BettererTasksLogger name="Betterer" update={update} exit={false} done={done} timer={!precommit && !ci}>
           {suite.runs.map((run) => (
             <BettererTaskLogger key={run.name} name={run.name} task={useTask(run)} />
           ))}
