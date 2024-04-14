20dc37b9ce94172f7e79a653bd7e191ba9a4ed24
Craig Spence <craigspence0@gmail.com>
2024-04-14 19:58:17 +0200

    refactor(tooling 🔧): move to npm

--- packages/betterer/src/test/loader.ts
+++ packages/betterer/src/test/loader.ts
@@ -28,7 +28,6 @@ function loadTestMetaFromConfig(configPath: string): BettererTestFactoryMetaMap 
     });
     return testMeta;
   } catch (error) {
-    debugger;
     throw new BettererError(`could not import config from "${configPath}". 😔`, error as BettererError);
   }
 }
