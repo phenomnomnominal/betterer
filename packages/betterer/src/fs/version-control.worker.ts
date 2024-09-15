73beb35534ab922d89cfb827003af15136224765
Craig Spence <craigspence0@gmail.com>
2024-09-15 22:37:32 +1200

    feat(betterer ✨)!: allow cache in CI mode
    BREAKING CHANGE: ci: true no longer disables the cache

--- packages/betterer/src/fs/version-control.worker.ts
+++ packages/betterer/src/fs/version-control.worker.ts
@@ -10,6 +10,7 @@ import { BettererFileCacheΩ } from './file-cache.js';
 let versionControl: BettererVersionControl | null = null;
 let cache: BettererFileCache | null = null;
 
+/** @knipignore part of worker API */
 export async function init(config: BettererConfig): Promise<void> {
   if (config.cache) {
     cache = await BettererFileCacheΩ.create(config.cachePath, config.configPaths);
@@ -17,41 +18,49 @@ export async function init(config: BettererConfig): Promise<void> {
   versionControl = await BettererGitΩ.create(cache, config.versionControlPath);
 }
 
+/** @knipignore part of worker API */
 export function add(resultsPath: string): Promise<void> {
   checkInitialisedVersionControl(versionControl);
   return versionControl.add(resultsPath);
 }
 
+/** @knipignore part of worker API */
 export function filterIgnored(filePaths: BettererFilePaths): BettererFilePaths {
   checkInitialisedVersionControl(versionControl);
   return versionControl.filterIgnored(filePaths);
 }
 
+/** @knipignore part of worker API */
 export function getFilePaths(): BettererFilePaths {
   checkInitialisedVersionControl(versionControl);
   return versionControl.getFilePaths();
 }
 
+/** @knipignore part of worker API */
 export function sync(): Promise<void> {
   checkInitialisedVersionControl(versionControl);
   return versionControl.sync(cache);
 }
 
+/** @knipignore part of worker API */
 export function clearCache(testName: string): void {
   checkInitialisedCache(cache);
   cache.clearCache(testName);
 }
 
+/** @knipignore part of worker API */
 export function filterCached(testName: string, filePaths: BettererFilePaths): BettererFilePaths {
   checkInitialisedCache(cache);
   return cache.filterCached(testName, filePaths);
 }
 
+/** @knipignore part of worker API */
 export function updateCache(testName: string, filePaths: BettererFilePaths): void {
   checkInitialisedCache(cache);
   cache.updateCache(testName, filePaths);
 }
 
+/** @knipignore part of worker API */
 export function writeCache(): Promise<void> {
   checkInitialisedCache(cache);
   return cache.writeCache();
