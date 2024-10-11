845ecaccc9e8dac58cb37f128caf8966636367ab
Craig Spence <craigspence0@gmail.com>
2024-10-11 15:20:41 +1300

    feat(betterer ✨): improve CLI output when files have changed but test results have not

--- packages/reporter/src/components/suite/WorkflowSuggestions.tsx
+++ packages/reporter/src/components/suite/WorkflowSuggestions.tsx
@@ -8,7 +8,7 @@ export const WorkflowSuggestions: FC = () => {
     <Box marginTop={1} flexDirection="column">
       <Text>{workflowSuggestions()}</Text>
       <Box marginTop={1} marginLeft={4}>
-        <Link url={workflowLink()}>
+        <Link url={workflowLink()} fallback={process.env.TEST !== 'true'}>
           <Text color="yellowBright">Workflow Documentation</Text>
         </Link>
       </Box>
