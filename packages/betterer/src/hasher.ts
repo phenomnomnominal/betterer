99011f1ae3843402b1be7ccaa425c0bf8fe0b496
Craig Spence <craigspence0@gmail.com>
2024-10-12 17:36:13 +1300

    fix(betterer 🐛): just use the string as the memoize key

--- packages/betterer/src/hasher.ts
+++ packages/betterer/src/hasher.ts
@@ -1,8 +1,17 @@
 import djb2a from 'djb2a';
 import memoize from 'fast-memoize';
 
-import { normaliseNewlines } from './utils.js';
+import { isString, normaliseNewlines } from './utils.js';
+import { invariantΔ } from '@betterer/errors';
 
-export const createHash = memoize(function createHash(value: string): string {
-  return djb2a(normaliseNewlines(value)).toString();
-});
+export const createHash = memoize(
+  function createHash(value: string): string {
+    return djb2a(normaliseNewlines(value)).toString();
+  },
+  {
+    serializer: (value: unknown) => {
+      invariantΔ(isString(value), '`createHash` should only be called with string arguments!');
+      return value;
+    }
+  }
+);
